ff78ae9b084cebf72d3e7f0eeb95edd32b6afad8
Zapodask <joaopedroemerim@gmail.com>
2022-04-01 01:24:32 -0300

    lose conditions

--- script.js
+++ script.js
@@ -2,59 +2,57 @@ class Game {
     constructor() {
         this.canvas = document.getElementById('keyboard')
         this.ctx = this.canvas.getContext('2d')
-        this.snake = [{
-            x: 60,
-            y: 60
-        }, {
-            x: 60,
-            y: 60
-        }, {
-            x: 60,
-            y: 60
-        }]
+        this.snake = []
         this.snake_area = 20
         this.direction = 'right'
         this.moved = false
 
+        this.snake.push([60, 60], [60, 60], [60, 60])
 
-        setInterval(() => this.init(), 1000)
+        this.interval = setInterval(() => this.init(), 1000)
     }
 
     init() {
         this.move()
-        this.ctx.clearRect(0, 0,500, 500)
+
+        this.ctx.clearRect(0, 0, 500, 500)
         for (let pos of this.snake) {
             this.ctx.fillStyle = '#77f'
-            this.ctx.fillRect(pos.x, pos.y, this.snake_area - 1, this.snake_area - 1)
+            this.ctx.fillRect(pos[0], pos[1], this.snake_area - 1, this.snake_area - 1)
         }
+        
         this.moved = false
     }
 
     move() {
         switch (this.direction) {
             case 'right':
-                this.update_snake({
-                    x: this.snake[0].x + this.snake_area,
-                    y: this.snake[0].y
-                })
+                this.update_snake([
+                    this.snake[0][0] + this.snake_area,
+                    this.snake[0][1]
+                ])
+                this.lose_conditions()
                 break
             case 'left':
-                this.update_snake({
-                    x: this.snake[0].x - this.snake_area,
-                    y: this.snake[0].y
-                })
+                this.update_snake([
+                    this.snake[0][0] - this.snake_area,
+                    this.snake[0][1]
+                ])
+                this.lose_conditions()
                 break
             case 'up':
-                this.update_snake({
-                    x: this.snake[0].x,
-                    y: this.snake[0].y - this.snake_area
-                })
+                this.update_snake([
+                    this.snake[0][0],
+                    this.snake[0][1] - this.snake_area
+                ])
+                this.lose_conditions()
                 break
             case 'down':
-                this.update_snake({
-                    x: this.snake[0].x,
-                    y: this.snake[0].y + this.snake_area
-                })
+                this.update_snake([
+                    this.snake[0][0],
+                    this.snake[0][1] + this.snake_area
+                ])
+                this.lose_conditions()
                 break
         }
     }
@@ -62,6 +60,29 @@ class Game {
     update_snake(position) {
         this.snake.pop()
         this.snake.unshift(position)
+    }
+
+    lose_conditions() {
+        try {
+            let head = this.snake[0]
+
+            // Check if snake is out of bounds
+            if (head[0] < 0 || head[0] >= 500 || head[1] < 0 || head[1] >= 500) {
+                throw ''
+            }
+    
+            let snake_body = this.snake.slice(1)
+    
+            // Check if snake is eating itself
+            for (let i in snake_body) {
+                if (this.snake[0].every((val, index) => val === snake_body[i][index]) === true) {
+                    throw ''
+                }
+            }
+        } catch (e) {
+            alert('You lose')
+            clearInterval(this.interval)
+        }
     }
 }
 
