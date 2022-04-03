e43683d3c8314b9f8ffba852a95a6c2863fca4ae
Zapodask <joaopedroemerim@gmail.com>
2022-04-03 17:49:56 -0300

    high score

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const setHighScore = () => document.getElementById('high_score').innerHTML = localStorage.getItem('high_score') || 0
+
 class Game {
     constructor() {
         this.canvas = document.getElementById('keyboard')
@@ -7,10 +9,11 @@ class Game {
         this.direction = 'right'
         this.moved = false
         this.food = []
+        this.score = 0
 
         this.generate_food()
 
-        this.interval = setInterval(() => this.init(), 1000)
+        this.interval = setInterval(() => this.init(), 500)
     }
 
     init() {
@@ -19,17 +22,20 @@ class Game {
         this.lose_conditions()
         this.check_food()
 
+        // Generate snake body
         this.ctx.clearRect(0, 0, 500, 500)
         for (let pos of this.snake) {
             this.create_rect('#77f', '#000', pos[0], pos[1])
         }
 
+        // Generate food
         this.create_rect('#f44', '#050', this.food[0], this.food[1])
         
         this.moved = false
     }
 
     create_rect(color, border_color, x, y) {
+        // Create square with border
         this.ctx.fillStyle = border_color
         this.ctx.fillRect(x, y, this.snake_area, this.snake_area)
 
@@ -38,6 +44,7 @@ class Game {
     }
 
     move() {
+        // Move snake
         switch (this.direction) {
             case 'right':
                 this.update_snake([
@@ -67,6 +74,7 @@ class Game {
     }
 
     update_snake(position) {
+        // Update snake position
         this.snake.pop()
         this.snake.unshift(position)
     }
@@ -91,26 +99,51 @@ class Game {
         } catch (e) {
             alert('You lose')
             clearInterval(this.interval)
+            startBtn.disabled = false
+
+            if (this.score > localStorage.getItem('high_score')) {
+                localStorage.setItem('high_score', this.score)
+            }
+
+            setHighScore()
         }
     }
 
     generate_food() {
+        // Generate random food
         this.food[0] = Math.floor(Math.random() * (500 / this.snake_area)) * this.snake_area
         this.food[1] = Math.floor(Math.random() * (500 / this.snake_area)) * this.snake_area
+
+        // Check if food is on snake
+        for (let i in this.snake) {
+            if (this.snake[i].every((val, index) => val === this.food[index]) === true) {
+                this.generate_food()
+            }
+        }
     }
 
     check_food() {
+        // Check if snake ate food
         if (this.snake[0].every((val, index) => val === this.food[index]) === true) {
             this.generate_food()
             this.snake.push(this.snake[this.snake.length - 1])
+            this.score += 1
+            document.getElementById('score').innerHTML = this.score
         }
     }
 }
 
-const game = new Game()
+let game
+
+const startBtn = document.getElementById('start')
+
+startBtn.addEventListener('click', () => {
+    startBtn.disabled = true
+    game = new Game()
+})
 
 document.addEventListener('keydown', (e) => {
-    if (game.moved === false) {
+    if (game && game.moved === false) {
         switch (e.key) {
             case 'ArrowRight':
                 if (game.direction !== 'left' && game.direction !== 'right') {
@@ -139,3 +172,5 @@ document.addEventListener('keydown', (e) => {
         }
     }
 })
+
+setHighScore()
